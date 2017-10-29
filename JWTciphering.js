2f9e0bf4d12696b3f9cafb6f921eae082a4f3bbd
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-10-29 11:23:14 -0600

    Test of JWTCiphering

---
+++ JWTciphering.js
@@ -0,0 +1,27 @@
+var jwt = require('jwt-simple');
+var secret = 'secretword';
+var json = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+var token = jwt.encode(json, secret);
+console.log(token);
